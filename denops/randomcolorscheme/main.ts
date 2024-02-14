025f63bcfa77a0b68bce5a59a7f6fa93b6a856d7
GitHub <noreply@github.com>
2024-02-14 06:06:56 +0000

    chore(deps): Update deno dependencies

--- denops/randomcolorscheme/main.ts
+++ denops/randomcolorscheme/main.ts
@@ -21,7 +21,7 @@ import {
   join,
   normalize,
 } from "https://deno.land/std@0.215.0/path/mod.ts";
-import { ensure, is } from "https://deno.land/x/unknownutil@v3.16.0/mod.ts";
+import { ensure, is } from "https://deno.land/x/unknownutil@v3.16.1/mod.ts";
 import { parse, stringify } from "https://deno.land/std@0.215.0/toml/mod.ts";
 import { filterEntries } from "https://deno.land/std@0.215.0/collections/filter_entries.ts";
 import { mapEntries } from "https://deno.land/std@0.215.0/collections/map_entries.ts";
