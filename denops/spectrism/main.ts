dfa653c51c62447b4234a9c57a65a08c2cb1d511
GitHub <noreply@github.com>
2024-03-09 06:07:08 +0000

    chore(deps): Update deno dependencies

--- denops/spectrism/main.ts
+++ denops/spectrism/main.ts
@@ -12,20 +12,20 @@ import * as op from "https://deno.land/x/denops_std@v6.3.0/option/mod.ts";
 import * as vars from "https://deno.land/x/denops_std@v6.3.0/variable/mod.ts";
 import type { Denops } from "https://deno.land/x/denops_std@v6.3.0/mod.ts";
 import xdg from "https://deno.land/x/xdg@v10.6.0/src/mod.deno.ts";
-import { delay } from "https://deno.land/std@0.218.2/async/delay.ts";
-import { walk } from "https://deno.land/std@0.218.2/fs/walk.ts";
+import { delay } from "https://deno.land/std@0.219.1/async/delay.ts";
+import { walk } from "https://deno.land/std@0.219.1/fs/walk.ts";
 import {
   basename,
   dirname,
   extname,
   join,
   normalize,
-} from "https://deno.land/std@0.218.2/path/mod.ts";
+} from "https://deno.land/std@0.219.1/path/mod.ts";
 import { ensure, is } from "https://deno.land/x/unknownutil@v3.17.0/mod.ts";
-import { parse, stringify } from "https://deno.land/std@0.218.2/toml/mod.ts";
-import { filterEntries } from "https://deno.land/std@0.218.2/collections/filter_entries.ts";
-import { mapEntries } from "https://deno.land/std@0.218.2/collections/map_entries.ts";
-import { ensureDir } from "https://deno.land/std@0.218.2/fs/mod.ts";
+import { parse, stringify } from "https://deno.land/std@0.219.1/toml/mod.ts";
+import { filterEntries } from "https://deno.land/std@0.219.1/collections/filter_entries.ts";
+import { mapEntries } from "https://deno.land/std@0.219.1/collections/map_entries.ts";
+import { ensureDir } from "https://deno.land/std@0.219.1/fs/mod.ts";
 import Chance from "https://cdn.skypack.dev/chance@1.1.11/";
 
 const defaultPriority = 100;
